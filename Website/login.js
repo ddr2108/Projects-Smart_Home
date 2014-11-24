6e9f2b4c21fabf94c43c6ae716e9693c90c3e340
Deep Datta Roy <ddattaro@cisco.com>
2014-11-24 15:45:21 -0500

    Added login page

--- Website/login.js
+++ Website/login.js
@@ -1,3 +1,69 @@
+/********************************
+* main
+* -------
+* Beginning of prgoram
+*
+*
+* params:
+*	none
+********************************/
 function main(){
+	//create form
+	var form = document.createElement("form");
+	form.setAttribute('method',"get");
+
+	//create username field
+	var unField = document.createElement("input"); //input element, text
+	unField.setAttribute('type',"text");
+	unField.setAttribute('name',"username");
+
+	//create password field
+	var pwField= document.createElement("input"); //input element, text
+	pwField.setAttribute('type',"text");
+	pwField.setAttribute('name',"password");
+
+	//create submit button
+	var submitButton = document.createElement("input"); //input element, Submit button
+	submitButton.setAttribute('type',"button");
+	submitButton.setAttribute('value',"Log In");
+	submitButton.setAttribute('onclick',"checkLogin();");
+
+	//add items to form
+	form.appendChild(unField);
+	form.appendChild(pwField);
+	form.appendChild(submitButton);
+
+	//add to page
+	document.body.appendChild(form);
+}
+
+/********************************
+* checkLogin
+* -------
+* Check the login info
+*
+*
+* params:
+*	none
+********************************/
+function checkLogin(){
+		document.body.innerHTML = "";
+
+	//begin setup for user
+	beginSetup();
+}
+
+/********************************
+* beginSetup
+* -------
+* Begin the setup for user
+*
+*
+* params:
+*	none
+********************************/
+function beginSetup(){
+
+	//set up the page
 	setup();
 }
---
+++ Website/logout.js
@@ -0,0 +1,66 @@
+//back button
+var imageLogout = "images\\navigation\\back.png";
+/////////////////////////////////////////////////////
+
+/********************************
+* back
+* -------
+* set up an object to represent
+* a light
+*
+*
+* params:
+*	id - unique ID for light
+*	name - name of light
+*	powered - state of light
+********************************/
+function back(id, name){
+	//set up parameters for this object
+	this.id = id;	
+	this.name = name;
+	this.image = new Image();
+}
+
+/********************************
+* setUpImageBack
+* -------
+* Sets up image on screen for going back
+*
+*
+* params:
+*	obj - object modified
+********************************/
+function setUpImageBack(obj){
+	//create a div to hold image
+	var div = document.createElement("div");
+	div.id = obj.id;
+	document.body.appendChild(div);
+
+	//add image for this light
+	obj.image.src = imageBack;
+	obj.image.id = "image";
+	obj.image.onclick = function(){goHome(obj);};
+	document.getElementById(obj.id).appendChild(obj.image);
+	
+	//add title to image
+	var p = document.createElement("p");
+	p.innerHTML = obj.name;
+	p.id = "title";
+	document.getElementById(obj.id).appendChild(p);
+}
+
+/********************************
+* goHome
+* -------
+* Go to home screen
+*
+*
+* params:
+*	obj - object modified
+********************************/
+function goHome(obj){
+	//display home controllers
+	displayControllers()
+}
+
+var back = new back('back', 'Back');
